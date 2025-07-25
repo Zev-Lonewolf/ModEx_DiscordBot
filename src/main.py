007fa110527f05337804c21a89766c7d5987677a
Zev Lonewolf <gleidsongonzagasilva@gmail.com>
2025-07-25 17:05:26 -0400

    Update 2025.07.25 -
    Fixed several errors and restored "eaten" lines. Variables are now in Pt-BR as well as the origin of the project.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from config import TOKEN, PREFIX
 from discord.ui import View, Button
 import json
-import os
 from embed import (
     get_language_embed,
     get_greeting_embed,
@@ -17,11 +16,9 @@ intents.reactions = True
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f"Usuário conectado: {bot.user}!")
+mensagem_idioma_id = {}
 
 def carregar_idiomas():
     try:
@@ -37,11 +34,23 @@ def salvar_idiomas(dados):
 idiomas = carregar_idiomas()
 
 def obter_idioma(guild_id):
-    return idiomas.get(str(guild_id), "en")
+    guild_id = str(guild_id)
+    if guild_id not in idiomas:
+        idiomas[guild_id] = "en"
+        salvar_idiomas(idiomas)
+    return idiomas[guild_id]
 
 def definir_idioma(guild_id, idioma):
     idiomas[str(guild_id)] = idioma
     salvar_idiomas(idiomas)
+
+@bot.before_invoke
+async def preparar_contexto(ctx):
+    ctx.language = obter_idioma(ctx.guild.id)
+
+@bot.event
+async def on_ready():
+    print(f"Usuário conectado: {bot.user}!")
 
 @bot.event
 async def on_guild_join(guild):
@@ -51,6 +60,8 @@ async def on_guild_join(guild):
             message = await channel.send(embed=embed)
             await message.add_reaction("🇺🇸")
             await message.add_reaction("🇧🇷")
+
+            mensagem_idioma_id[str(guild.id)] = message.id
             break
 
 @bot.event
@@ -58,9 +69,11 @@ async def on_raw_reaction_add(payload):
     if payload.member.bot:
         return
 
-    guild_id = payload.guild_id
-    guild = bot.get_guild(guild_id)
-    channel = bot.get_channel(payload.channel_id)
+    guild_id = str(payload.guild_id)
+    message_id = mensagem_idioma_id.get(guild_id)
+
+    if payload.message_id != message_id:
+        return
 
     if payload.emoji.name == "🇧🇷":
         definir_idioma(guild_id, "pt")
@@ -69,24 +82,28 @@ async def on_raw_reaction_add(payload):
     else:
         return
 
+    guild = bot.get_guild(payload.guild_id)
+    channel = bot.get_channel(payload.channel_id)
     idioma = obter_idioma(guild_id)
     embed = get_greeting_embed(idioma)
     await channel.send(embed=embed)
 
-@bot.command(name="setup", aliases= ["Setup", "SETUP"])
+@bot.command(name="setup", aliases=["Setup", "SETUP"])
 async def setup(ctx):
     idioma = obter_idioma(ctx.guild.id)
     embed = get_setup_embed(idioma)
     await ctx.send(embed=embed)
 
-@bot.command(name="idioma", aliases= ["Idioma", "IDIOMA", "language", "Language", "LANGUAGE"])
+@bot.command(name="idioma", aliases=["Idioma", "IDIOMA", "language", "Language", "LANGUAGE"])
 async def idioma(ctx):
     embed = get_language_embed()
     message = await ctx.send(embed=embed)
     await message.add_reaction("🇺🇸")
     await message.add_reaction("🇧🇷")
 
-@bot.command(name="sobre", aliases= ["Sobre", "SOBRE", "about", "About", "ABOUT"])
+    mensagem_idioma_id[str(ctx.guild.id)] = message.id
+
+@bot.command(name="sobre", aliases=["Sobre", "SOBRE", "about", "About", "ABOUT"])
 async def sobre(ctx):
     idioma = obter_idioma(ctx.guild.id)
     embed = get_about_embed(idioma)
